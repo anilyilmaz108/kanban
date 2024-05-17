3206da255cc904df31f036756b7f386da48c95d2
Anıl Yılmaz <anilyilmaz108@gmail.com>
2024-05-17 21:16:32 +0300

    Environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,16 +17,7 @@ import {
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { PublicComponent } from './layouts/public/public.component';
 import { HttpClientModule } from '@angular/common/http';
-
-const firebaseConfig = {
-  projectId: 'kanban-1b2c1',
-  appId: '1:858095447541:web:6a93e8ad1d655d6838c4bc',
-  storageBucket: 'kanban-1b2c1.appspot.com',
-  apiKey: 'AIzaSyA9l5MPa9nWYTVbpijKpajT02CnNNEUpCU',
-  authDomain: 'kanban-1b2c1.firebaseapp.com',
-  messagingSenderId: '858095447541',
-  measurementId: 'G-K8K172DDNX',
-};
+import { firebaseConfig } from './environment/environment';
 
 @NgModule({
   declarations: [AppComponent, PrivateComponent, PublicComponent],
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+        projectId: 'kanban-1b2c1',
+        appId: '1:858095447541:web:6a93e8ad1d655d6838c4bc',
+        storageBucket: 'kanban-1b2c1.appspot.com',
+        apiKey: 'AIzaSyA9l5MPa9nWYTVbpijKpajT02CnNNEUpCU',
+        authDomain: 'kanban-1b2c1.firebaseapp.com',
+        messagingSenderId: '858095447541',
+        measurementId: 'G-K8K172DDNX',
+};
